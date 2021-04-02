190e28a6524967157d7e1d8de6a02303f1f6e02a
Moritz Schmidt <fusselwurm@gmail.com>
2021-04-03 00:14:07 +0200

    move everything into a Parser class, with raw & options as state

--- src/class-parser.ts
+++ src/class-parser.ts
@@ -19,272 +19,303 @@ export interface Options {
     }
 }
 
-export const parse = function (raw: string, options?: Options): any {
-    let currentPosition: number = 0;
-    let current = function (): string {
-        return raw[currentPosition] || '';
-    };
-    let translateString = function(string: string): string {
-        if(typeof options.translations === "object") {
-            return options.translations.hasOwnProperty(string) ?
-                options.translations[string] : string;
+function indexOfOrMaxInt(str: string, fromPos: number) {
+    const pos = this.indexOf(str, fromPos);
+    return pos === -1 ? Infinity : pos;
+}
+
+function isValidVarnameChar(char): boolean {
+    return (char >= '0' && char <= '9') ||
+        (char >= 'A' && char <= 'Z') ||
+        (char >= 'a' && char <= 'z') ||
+        char === '_';
+}
+
+class Parser {
+    private currentPosition: number = 0
+    private result = {};
+
+    constructor(
+        private raw: string,
+        private options: Options = {},
+        ) {
+    }
+
+    public parse(): any {
+        this.reset();
+
+        this.detectComment();
+        this.parseWhitespace();
+        while(this.current()) {
+            this.parseProperty(this.result);
+            this.parseWhitespace();
+        }
+
+        return this.result;
+    }
+
+
+    public reset() {
+        this.currentPosition = 0;
+        this.result = {};
+    }
+
+
+    private current(): string {
+        return this.raw[this.currentPosition] || '';
+    }
+
+    public translateString(string: string): string {
+        if(typeof this.options.translations === "object") {
+            return this.options.translations.hasOwnProperty(string) ?
+                this.options.translations[string] : string;
         }
 
         return string;
-    };
-    let indexOfOrMaxInt = function (str: string, fromPos: number) {
-        const pos = this.indexOf(str, fromPos);
-        return pos === -1 ? Infinity : pos;
-    };
-    let parseArray = function (): any[] {
+    }
+
+
+    private parseArray(): any[] {
         const result = [];
-        assert(current() === chars.CURLY_OPEN);
-        next();
-        parseWhitespace();
-        while (current() !== chars.CURLY_CLOSE) {
-            result.push(parseNonArrayPropertyValue());
-            parseWhitespace();
-            if (current() === chars.COMMA) {
-                next();
-                parseWhitespace();
+        this.assert(this.current() === chars.CURLY_OPEN);
+        this.next();
+        this.parseWhitespace();
+        while (this.current() !== chars.CURLY_CLOSE) {
+            result.push(this.parseNonArrayPropertyValue());
+            this.parseWhitespace();
+            if (this.current() === chars.COMMA) {
+                this.next();
+                this.parseWhitespace();
             } else {
                 break;
             }
 
         }
-        next();
+        this.next();
         return result;
-    };
-    let parseProperty = function (context): any {
-        let name = parsePropertyName(),
+    }
+    private parseProperty(context): any {
+        let name = this.parsePropertyName(),
             value;
 
-        parseWhitespace();
+        this.parseWhitespace();
 
         switch (name) {
             case 'class':
-                name = parsePropertyName();
-                parseWhitespace();
-                if (current() === ':') { // skip parent class declaration
-                    next();
-                    parseWhitespace();
-                    parsePropertyName();
-                    parseWhitespace();
+                name = this.parsePropertyName();
+                this.parseWhitespace();
+                if (this.current() === ':') { // skip parent class declaration
+                    this.next();
+                    this.parseWhitespace();
+                    this.parsePropertyName();
+                    this.parseWhitespace();
                 }
                 break;
             case 'delete': // skip delete statements
-                parsePropertyName();
-                parseWhitespace();
-                assert(current() === chars.SEMICOLON);
-                next();
+                this.parsePropertyName();
+                this.parseWhitespace();
+                this.assert(this.current() === chars.SEMICOLON);
+                this.next();
                 return;
             case 'import': // skip import statements
-                parseWhitespace();
-                parsePropertyName();
-                parseWhitespace();
-                assert(current() === chars.SEMICOLON);
-                next();
+                this.parseWhitespace();
+                this.parsePropertyName();
+                this.parseWhitespace();
+                this.assert(this.current() === chars.SEMICOLON);
+                this.next();
                 return;
         }
 
-        switch (current()) {
+        switch (this.current()) {
             case chars.SQUARE_OPEN:
-                assert(next() === chars.SQUARE_CLOSE);
-                next();
-                parseWhitespace();
-                assert(current() === chars.EQUALS);
-                next();
-                parseWhitespace();
-                value = parseArray();
+                this.assert(this.next() === chars.SQUARE_CLOSE);
+                this.next();
+                this.parseWhitespace();
+                this.assert(this.current() === chars.EQUALS);
+                this.next();
+                this.parseWhitespace();
+                value = this.parseArray();
                 break;
             case chars.EQUALS:
-                next();
-                parseWhitespace();
-                value = parseNonArrayPropertyValue();
+                this.next();
+                this.parseWhitespace();
+                value = this.parseNonArrayPropertyValue();
                 break;
             case chars.CURLY_OPEN:
-                value = parseClassValue();
+                value = this.parseClassValue();
                 break;
             default:
-                throw new Error('unexpected value at pos ' + currentPosition);
+                throw new Error('unexpected value at pos ' + this.currentPosition);
         }
 
         context[name] = value;
-        parseWhitespace();
-        assert(current() === chars.SEMICOLON);
-        next();
-    };
-    let parseWhitespace = function (): void {
-        while (isWhitespace()) {
-            next();
+        this.parseWhitespace();
+        this.assert(this.current() === chars.SEMICOLON);
+        this.next();
+    }
+    private parseWhitespace(): void {
+        while (this.isWhitespace()) {
+            this.next();
         }
-    };
-    let isWhitespace = function (): boolean {
-        return (' \t\r\n'.indexOf(raw[currentPosition]) !== -1) ||
-            (raw.charCodeAt(currentPosition) < 32);
-    };
-    let assert = function(bool: boolean, msg: string = ''): void {
-            if (bool) {
-                return;
-            }
-            throw new Error(
-                msg + ' at position ' + currentPosition + ', ' +
-                'before: ' + JSON.stringify(raw.substr(Math.max(0, currentPosition - 40), 40)) + ', ' +
-                'after: ' + JSON.stringify(raw.substr(currentPosition, 40))
-            );
-        },
-        detectComment = function(): void {
-            if (current() === chars.SLASH && raw[currentPosition + 1] === chars.SLASH) {
-                const indexOfLinefeed = raw.indexOf('\n', currentPosition);
-                currentPosition = indexOfLinefeed === -1 ? raw.length : indexOfLinefeed;
-            } else if(current() === chars.SLASH && raw[currentPosition + 1] === chars.ASTERISK) {
-                const multilineClose = chars.ASTERISK + chars.SLASH;
-                const indexOfCommentEnd = raw.indexOf(multilineClose, currentPosition + 2);
-                currentPosition = indexOfCommentEnd === -1 ? raw.length : indexOfCommentEnd + multilineClose.length;
-            }
-        },
-        next = function(): string {
-            currentPosition += 1;
-            detectComment();
-            return current();
-        },
-        nextWithoutCommentDetection = function(): string {
-            currentPosition += 1;
-            return current();
-        },
-        result = {},
-        weHaveADoubleQuote = function(): boolean {
-            return (raw.substr(currentPosition, 2).indexOf('""') === 0);
-        },
-        weHaveAStringLineBreak = function(): boolean {
-            return raw.substr(currentPosition, 6).indexOf('" \\n "') === 0;
-        },
-        forwardToNextQuote = function(): void {
-            currentPosition = indexOfOrMaxInt.call(raw, chars.QUOTE, currentPosition + 1);
-        },
-        parseString = function(): any {
-            let result = '';
-            assert(current() === chars.QUOTE);
-            nextWithoutCommentDetection();
-            while (current()) {
-                if (weHaveADoubleQuote()) {
-                    result += current();
-                    nextWithoutCommentDetection();
-                } else if (weHaveAStringLineBreak()) {
-                    result += '\n';
-                    next();
-                    forwardToNextQuote();
-                } else if (current() === chars.QUOTE) {
-                    break;
-                } else {
-                    result += current();
-                }
-                nextWithoutCommentDetection();
-            }
-            assert(current() === chars.QUOTE);
-            nextWithoutCommentDetection();
-            return result;
-        },
-        parseTranslationString = function(): string {
-            let result: string = '';
-            assert(current() === chars.DOLLAR);
-            next();
-            assert(
-                raw.substr(currentPosition, 3).indexOf('STR') === 0,
-                'Invalid translation string beginning'
-            );
-            while (current()) {
-                if (
-                    current() === chars.SEMICOLON
-                    || (current() === chars.COMMA || current() === chars.CURLY_CLOSE)
-                ) {
-                    break;
-                } else {
-                    if (isWhitespace()) {
-                        parseWhitespace();
-                        break;
-                    } else {
-                        result += current();
-                    }
-                }
-                nextWithoutCommentDetection();
-            }
-            assert(
-                current() === chars.SEMICOLON
-                || (current() === chars.COMMA || current() === chars.CURLY_CLOSE)
-            );
+    }
+    private isWhitespace (): boolean {
+        return (' \t\r\n'.indexOf(this.raw[this.currentPosition]) !== -1) ||
+            (this.raw.charCodeAt(this.currentPosition) < 32);
+    }
+    private assert(bool: boolean, msg: string = ''): void {
+        if (bool) {
+            return;
+        }
+        throw new Error(
+            msg + ' at position ' + this.currentPosition + ', ' +
+            'before: ' + JSON.stringify(this.raw.substr(Math.max(0, this.currentPosition - 40), 40)) + ', ' +
+            'after: ' + JSON.stringify(this.raw.substr(this.currentPosition, 40))
+        );
+    }
+    private detectComment(): void {
+        if (this.current() === chars.SLASH && this.raw[this.currentPosition + 1] === chars.SLASH) {
+            const indexOfLinefeed = this.raw.indexOf('\n', this.currentPosition);
+            this.currentPosition = indexOfLinefeed === -1 ? this.raw.length : indexOfLinefeed;
+        } else if(this.current() === chars.SLASH && this.raw[this.currentPosition + 1] === chars.ASTERISK) {
+            const multilineClose = chars.ASTERISK + chars.SLASH;
+            const indexOfCommentEnd = this.raw.indexOf(multilineClose, this.currentPosition + 2);
+            this.currentPosition = indexOfCommentEnd === -1 ? this.raw.length : indexOfCommentEnd + multilineClose.length;
+        }
+    }
+    private next(): string {
+        this.currentPosition += 1;
+        this.detectComment();
+        return this.current();
+    }
+    private nextWithoutCommentDetection(): string {
+        this.currentPosition += 1;
+        return this.current();
+    }
 
-            return translateString(result);
-        },
-        parseMathExpression = function() {
-            const posOfExpressionEnd = Math.min(
-                indexOfOrMaxInt.call(raw, chars.SEMICOLON, currentPosition),
-                indexOfOrMaxInt.call(raw, chars.CURLY_CLOSE, currentPosition),
-                indexOfOrMaxInt.call(raw, chars.COMMA, currentPosition)
-            );
-            const expression = raw.substr(currentPosition, posOfExpressionEnd - currentPosition);
-            assert(posOfExpressionEnd !== Infinity);
-            currentPosition = posOfExpressionEnd;
-
-            return +expression;
-        },
-        parseNonArrayPropertyValue = function(): any {
-            let result;
-
-            if (current() === chars.CURLY_OPEN) {
-                result = parseArray(); // on nested array property values
-            } else if (current() === chars.QUOTE) {
-                result = parseString();
-            } else if (current() === chars.DOLLAR) {
-                result = parseTranslationString();
+    private weHaveADoubleQuote(): boolean {
+        return (this.raw.substr(this.currentPosition, 2).indexOf('""') === 0);
+    }
+    private weHaveAStringLineBreak (): boolean {
+        return this.raw.substr(this.currentPosition, 6).indexOf('" \\n "') === 0;
+    }
+    private forwardToNextQuote(): void {
+        this.currentPosition = indexOfOrMaxInt.call(this.raw, chars.QUOTE, this.currentPosition + 1);
+    }
+    private parseString(): any {
+        let result = '';
+        this.assert(this.current() === chars.QUOTE);
+        this.nextWithoutCommentDetection();
+        while (this.current()) {
+            if (this.weHaveADoubleQuote()) {
+                result += this.current();
+                this.nextWithoutCommentDetection();
+            } else if (this.weHaveAStringLineBreak()) {
+                result += '\n';
+                this.next();
+                this.forwardToNextQuote();
+            } else if (this.current() === chars.QUOTE) {
+                break;
             } else {
-                result = parseMathExpression();
+                result += this.current();
             }
-            return result;
-        },
-        isValidVarnameChar = function(char): boolean {
-            return (char >= '0' && char <= '9') ||
-                (char >= 'A' && char <= 'Z') ||
-                (char >= 'a' && char <= 'z') ||
-                char === '_';
-        },
-        parsePropertyName = function(): string {
-            let result = current();
-            while (isValidVarnameChar(next())) {
-                result += current();
+            this.nextWithoutCommentDetection();
+        }
+        this.assert(this.current() === chars.QUOTE);
+        this.nextWithoutCommentDetection();
+        return result;
+    }
+
+    private parseTranslationString(): string {
+        let result: string = '';
+        this.assert(this.current() === chars.DOLLAR);
+        this.next();
+        this.assert(
+            this.raw.substr(this.currentPosition, 3).indexOf('STR') === 0,
+            'Invalid translation string beginning'
+        );
+        while (this.current()) {
+            if (
+                this.current() === chars.SEMICOLON
+                || (this.current() === chars.COMMA || this.current() === chars.CURLY_CLOSE)
+            ) {
+                break;
+            } else {
+                if (this.isWhitespace()) {
+                    this.parseWhitespace();
+                    break;
+                } else {
+                    result += this.current();
+                }
             }
-            return result;
-        },
-        parseClassValue = function(): any {
-            const result = {};
+            this.nextWithoutCommentDetection();
+        }
+        this.assert(
+            this.current() === chars.SEMICOLON
+            || (this.current() === chars.COMMA || this.current() === chars.CURLY_CLOSE)
+        );
 
-            assert(current() === chars.CURLY_OPEN);
-            next();
-            parseWhitespace();
+        return this.translateString(result);
+    }
 
-            while (current() !== chars.CURLY_CLOSE) {
+    private parseMathExpression(): number {
+        const posOfExpressionEnd = Math.min(
+            indexOfOrMaxInt.call(this.raw, chars.SEMICOLON, this.currentPosition),
+            indexOfOrMaxInt.call(this.raw, chars.CURLY_CLOSE, this.currentPosition),
+            indexOfOrMaxInt.call(this.raw, chars.COMMA, this.currentPosition)
+        );
+        const expression = this.raw.substr(this.currentPosition, posOfExpressionEnd - this.currentPosition);
+        this.assert(posOfExpressionEnd !== Infinity);
+        this.currentPosition = posOfExpressionEnd;
 
-                parseProperty(result);
-                parseWhitespace();
-            }
+        return +expression;
+    }
 
-            next();
+    private parseNonArrayPropertyValue(): any {
+        let result;
 
-            return result;
-        };
+        if (this.current() === chars.CURLY_OPEN) {
+            result = this.parseArray(); // on nested array property values
+        } else if (this.current() === chars.QUOTE) {
+            result = this.parseString();
+        } else if (this.current() === chars.DOLLAR) {
+            result = this.parseTranslationString();
+        } else {
+            result = this.parseMathExpression();
+        }
+        return result;
+    }
 
-    options = options || {};
+    private parsePropertyName(): string {
+        let result = this.current();
+        while (isValidVarnameChar(this.next())) {
+            result += this.current();
+        }
+        return result;
+    }
+    private parseClassValue(): any {
+        const result = {};
 
+        this.assert(this.current() === chars.CURLY_OPEN);
+        this.next();
+        this.parseWhitespace();
+
+        while (this.current() !== chars.CURLY_CLOSE) {
+
+            this.parseProperty(result);
+            this.parseWhitespace();
+        }
+
+        this.next();
+
+        return result;
+    };
+}
+
+export const parse = function (raw: string, options?: Options): any {
     if (typeof raw !== 'string') {
         throw new TypeError('expecting string!');
     }
+    const parser = new Parser(raw, options || {})
 
-    detectComment();
-    parseWhitespace();
-    while(current()) {
-        parseProperty(result);
-        parseWhitespace();
-    }
-
-    return result;
+    return parser.parse();
 };
